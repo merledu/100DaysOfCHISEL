192b9e2ce7e2b92593f6b0289007527f02d75980
Hamna Mohiuddin <110234585+hamnamohi@users.noreply.github.com>
2022-10-07 10:49:23 +0500

    Update Bitwidthtest.scala

--- src/test/scala/day3/Bitwidthtest.scala
+++ src/test/scala/day3/Bitwidthtest.scala
@@ -5,7 +5,7 @@ import chiseltest._
 
 class Bitwidthtest extends FreeSpec with ChiselScalatestTester{
     "bitwidth Test" in { 
-        test(new bitwidth ()){ c=>
+        test(new Bitwidth ()){ c=>
         c.io.input.poke(4.U)
         c.clock.step(1)
         c.io.output.expect(4.S)}}}
