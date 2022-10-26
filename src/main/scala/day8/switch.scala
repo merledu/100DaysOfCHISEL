c4eccf36eff87b9477afaa606c68671877125ad7
Asghar Raza <74490337+Asghar-Raza@users.noreply.github.com>
2022-10-26 10:56:44 +0500

    Create switch.scala

---
+++ src/main/scala/day8/switch.scala
@@ -0,0 +1,36 @@
+package day8
+import chisel3._
+import chisel3.util._
+
+class Switch_IO extends Bundle{
+    val sel = Input(UInt(3.W))
+    val in_a = Input(UInt(32.W))
+    val in_b = Input(UInt(32.W))
+    val out = Output(UInt(2.W))
+}
+object operations{
+    val ADD = 0
+    val SUB = 1
+    val DIVIDE = 2
+    val MULTIPLY = 3
+}
+import operations._
+class SWITCH extends Module{
+    val io = IO(new Switch_IO)
+    io.out := 0.U
+    switch(io.sel){
+        is(ADD.U){
+            io.out := io.in_a + io.in_b
+        }
+        is(SUB.U){
+            io.out := io.in_a - io.in_b
+        }
+        is(DIVIDE.U){
+            io.out := io.in_a / io.in_b
+        }
+        is(MULTIPLY.U){
+            io.out := io.in_a * io.in_b
+        }
+    }
+    
+}
