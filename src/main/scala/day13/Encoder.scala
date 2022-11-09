fea3710ab338d10b141e5ba2de2b31d8dd5d11e0
pythonesta <61948628+zeeshan3534@users.noreply.github.com>
2022-11-09 12:14:26 +0500

    Create Encoder.scala

---
+++ src/main/scala/day13/Encoder.scala
@@ -0,0 +1,28 @@
+package day13
+import chisel3._
+import chisel3.util._
+
+
+class EncoderIO extends Bundle {
+val in = Input ( UInt(4.W) )
+val out = Output ( UInt(2.W))
+}
+
+class Encoder4to2 extends Module {
+val io = IO ( new EncoderIO)
+io.out := "b11".U
+switch(io.in){
+    is("b0001".U ){
+        io.out := "b00".U
+    }
+    is("b0010".U){
+        io . out := "b01".U
+
+    }
+    is("b0100".U ){
+        io.out := "b10".U
+    }
+}
+
+
+}
