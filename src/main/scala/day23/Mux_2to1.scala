83e354c23002891b93ce66b783243e04783cf4ac
Abdul Rehman <syle00rehman0@gmail.com>
2023-10-04 00:49:11 +0500

    Added day 23

--- src/main/scala/day23/Mux_2to1.scala
+++ src/main/scala/day23/Mux_2to1.scala
@@ -2,6 +2,7 @@ package day23
 import chisel3._
 // Mux IO interface
 class Mux_2to1_IO extends Bundle {
+  //input outputs
   val in_A = Input ( UInt(32.W ) )
   val in_B = Input ( UInt(32.W ) )
   val select = Input ( Bool () )
@@ -10,7 +11,9 @@ class Mux_2to1_IO extends Bundle {
 // 2 to 1 Mux implementation
 class Mux_2to1 extends Module {
   val io = IO (new Mux_2to1_IO )
-
+  //Mux returns the inputs depending upon the select input
+  // when select = 1 A is returned
+  // when select = 0 B is returned
   io.out := Mux ( io.select , io.in_A , io.in_B )
 
 }
--- src/test/scala/day23/Mux_2to1test.scala
+++ src/test/scala/day23/Mux_2to1test.scala
@@ -8,10 +8,10 @@ import chisel3.experimental.BundleLiterals._
 class Mux_2to1test extends FreeSpec with ChiselScalatestTester{
   "lab 2 ex1Tester " in {
     test(new Mux_2to1){ a=>
-      a.io.in_A.poke(45.U)
-      a.io.in_B.poke(55.U)
-      a.io.select.poke(1.B)
-      a.io.out.expect(45.U)
+      a.io.in_A.poke(45.U) // input A
+      a.io.in_B.poke(55.U) // input B
+      a.io.select.poke(1.B) // select pin selects which input to send as output
+      a.io.out.expect(45.U) // output received
       a.clock.step(4)
 
     }
